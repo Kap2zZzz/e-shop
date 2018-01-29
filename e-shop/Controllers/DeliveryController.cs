881a19be212e2e74bf0e5102d648cd0af6b22c27
Kap2zZzz <33594793+Kap2zZzz@users.noreply.github.com>
2018-01-29 21:17:06 +0200

    work Delivery

---
+++ e-shop/Controllers/DeliveryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_shop.Controllers
+{
+    public class DeliveryController : Controller
+    {
+        // GET: Delivery
+        public ActionResult Delivery()
+        {
+            @ViewBag.IsActiveDelivery = "active";
+            return View();
+        }
+    }
+}
